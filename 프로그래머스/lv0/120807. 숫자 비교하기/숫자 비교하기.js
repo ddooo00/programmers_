05a894583d6650b9314a17b11b6512a04498400d
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2023-09-20 17:35:44 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,10 +1,6 @@
 function solution(num1, num2) {
-    var answer = ""
+
     if(num1 ===num2){
-        return 1
-    }else if(num1 !==num2){
-        return -1
-    }
-    
-    return answer;
+return 1}else {return -1}
+
 }
