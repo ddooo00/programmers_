9d8db79897691de9b0a94c5819254b196f408f2c
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2023-10-16 22:02:05 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 24.73 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,9 +1,11 @@
-function solution(n) {
-    var answer = 0;
-    for(let i = 1; i <= n; i++){
-        if(n % i ===0){
-            answer++
+function solution(n) {    
+    let answer = [];
+    
+    for(let i = 1; i <= n / 2; i++){
+        if(n % i === 0){
+           answer.push(i);
         }
     }
-    return answer;
+    
+    return answer.length + 1;
 }
