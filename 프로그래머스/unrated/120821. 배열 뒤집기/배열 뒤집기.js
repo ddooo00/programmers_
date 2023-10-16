cd173e6bd85a8f6a36cb07fcb1293e47985be9af
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2023-10-16 22:17:20 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,3 +1,10 @@
 function solution(num_list) {
-return num_list.reverse();
+    // 새로운 배열 생성
+    const new_list = [];
+    // 새로운 배열에 num_list 거꾸로 하나씩 push
+    for (var i = num_list.length - 1; i >= 0; i --) {
+      new_list.push(num_list[i]);  
+    }
+    // 새로운 배열 출력
+    return new_list;
 }
