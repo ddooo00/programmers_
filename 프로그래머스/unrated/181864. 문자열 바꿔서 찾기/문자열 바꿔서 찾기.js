02eae3dfaec3323ebb22db2adec54f277027854d
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2023-11-01 19:09:07 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,20 +1,4 @@
 function solution(myString, pat) {
-  // "A"와 "B"를 동시에 변환
-  let trans = "";
-  for (let i = 0; i < myString.length; i++) {
-    if (myString[i] === "A") {
-      trans += "B";
-    } else if (myString[i] === "B") {
-      trans += "A";
-    } else {
-      trans += myString[i];
-    }
-  }
-  
-  // 변환된 문자열에서 원하는 패턴 검사
-  if (trans.includes(pat)) {
-    return 1;
-  } else {
-    return 0;
-  }
+  const trans = myString.split('').map(char => (char === 'A' ? 'B' : char === 'B' ? 'A' : char)).join('');
+  return trans.includes(pat) ? 1 : 0;
 }
