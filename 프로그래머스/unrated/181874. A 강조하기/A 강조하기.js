3aa31749ba32b540e024c4304a1952b659f82fbc
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2023-11-07 22:07:20 +0900

    [level unrated] Title: A 강조하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,14 +1,3 @@
 function solution(myString) {
-    let answer = '';
-    for(let i =0; i<myString.length; i++){
-        let char = myString[i];
-if(char === 'a'){
-    answer+= "A"
-}else if(char > "A" && char < "Z"){
-    answer += char.toLowerCase();
-}else{
-    answer+=char;
-}
-    }
-    return answer;
+    return [...myString].map(str => ['a', 'A'].includes(str)? 'A' : str.toLowerCase()).join('');
 }
